0fa462916ea5edc89a11742ecfe2139d59c07801
sztxr <sztxr@github.com>
2019-10-19 19:58:18 -0300

    uibuild

---
+++ build/precache-manifest.6d1306b142dbd04520b0e1e9b472ae16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "932b07ddbd0fb7b2054d950ae740c447",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b51c32ef0915d7aeca0",
+    "url": "/static/css/main.38732954.chunk.css"
+  },
+  {
+    "revision": "e332c93aa78c14e97d60",
+    "url": "/static/js/2.bdcaf41a.chunk.js"
+  },
+  {
+    "revision": "3b51c32ef0915d7aeca0",
+    "url": "/static/js/main.3e26472a.chunk.js"
+  },
+  {
+    "revision": "c33ef9ff59bdcc96faf2",
+    "url": "/static/js/runtime-main.09018f05.js"
+  }
+]);
